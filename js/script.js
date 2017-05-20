f86b58a28ecd604d9df391e7bfffd0e5548e9a3e
Melissa Wong <mellymai@gmail.com>
2017-05-20 13:11:59 -0700

    Styling updates

--- js/script.js
+++ js/script.js
@@ -18,19 +18,23 @@ $(function() {
         return false;
     });
 
-    //when .expand clicked...
-    $itemExpand.on('click', function() {
-        //toggle #sub-menu
-        $subMenu.slideToggle();
-        return false;
-    });
+    if (window.innerWidth < 640) {
+         //when #menu-item-38 clicked...
+        $itemExpand.on('click', function() {
+            //toggle #sub-menu
+            $subMenu.slideToggle();
+            return false;
+        }); 
+    }
+
+   
 
     if (window.innerWidth > 640) {
         $itemExpand.on('mouseenter', function() {
             $subMenu.slideDown();
             return false;
         });
-        $subMenu.on('mouseleave', function(){
+        $itemExpand.on('mouseleave', function(){
             $subMenu.slideUp('slow');
             return false;
         });
