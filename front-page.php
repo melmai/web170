4fc4d115a4d1b9696ff9ffcb5748c280c430f8b3
Melissa Wong <mellymai@gmail.com>
2017-05-20 19:07:50 -0700

    Creating a Static Home Page with Multiple Loops

--- front-page.php
+++ front-page.php
@@ -1,16 +1,77 @@
-<?php
+<?php get_header(); ?>
 
-get_header();
+<main>
+        <!--
+        <section class="container">
+            <button class="w3-button w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+            <img class="mySlides" src="images/slide1.svg">
+            <img class="mySlides" src="img_lights.jpg">
+            <img class="mySlides" src="img_mountains.jpg">
+            <img class="mySlides" src="img_forest.jpg">
+            <button class="w3-button w3-display-right" onclick="plusDivs(+1)">&#10095;</button>
+        </section>
+    -->
 
-if(have_posts()) : while(have_posts()) : the_post(); ?>
+        <section class="container">
+            <?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
+            <?php the_content(''); ?>
+            <?php endwhile; endif; ?>
+        </section>
 
-    <main>
-        
-        <?php the_content(); ?>
-        <?php endwhile; endif;?>
+        <section class="container">
+            <h2>Why Mobile Personal Training?</h2>
+            <p>With Progressive Gym, the world is your gym! We can train you at your home, your business, or public spaces. We will bring the gym to you! </p>
 
-        <small>front-page.php</small>
-    </main>
+            <p>Our mission: To transform your body and your approach to fitness.</p>
+        </section>
 
+        <section class="container">
+            <h2>How To Get Started</h2>
+            <section>
+                <div class="icon-box">
+                    <i class="fa fa-map-marker fa-3x" aria-hidden="true"></i>
+                </div> <!-- .icon-box -->
+                <div class="content">
+                    <h3 class="icon-box-labels">Step 1: Location</h3>
+                    <p>With Progressive Gym, the world is your gym! We can train you at your home, your business, or in public spaces. We will bring the gym to you!</p>
+                    <a href="locations.html" class="button">See Locations</a>
+                </div><!-- .content -->
+
+            </section>
+
+        <section>
+            <div class="icon-box">
+                <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+            </div> <!-- .icon-box -->
+            <div class="content">
+                <h3 class="icon-box-labels">Step 2: Select Style</h3>
+                <p>Whether you're looking for one-on-one sessions or corporate events, we accomodate groups of all sizes. We provide training on fundamental office ergonomics and pilates to bootcamp classes if that's what you prefer!</p>
+                <a href="services.html" class="button">See Styles</a>
+            </div><!-- .content -->
+        </section>
+
+        <section>
+            <div class="icon-box">
+                <i class="fa fa-pencil-square-o fa-3x" aria-hidden="true"></i>
+            </div><!-- .icon-box -->
+            <div class="content">
+                <h3 class="icon-box-labels">Step 3: Schedule</h3>
+                <p>Contact us to get your first session started! We travel within the greater Seattle area to fit your needs.</p>
+                <a href="contact.html" class="button">Contact</a>
+            </div><!-- .content -->
+        </section>
+    </section>
+
+    <section class="container">
+        <h2>Latest Blog Posts</h2>
+        <ul>
+            <?php rewind_posts(); ?>
+            <?php query_posts('showposts=3'); // ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+            <?php endwhile; // end loop two ?>
+        </ul>
+    </section>
+</main>
 
 <?php get_footer(); ?>
